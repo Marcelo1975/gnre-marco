c43dbdce22ad0b2d0c36f8af5f58a291775e47c1
Marcelo1975 <cecel.alex@gmail.com>
2019-08-18 16:12:32 -0300

    Erro SSL

--- index.php
+++ index.php
@@ -1,140 +1,69 @@
 <?php
-$xml = '<?xml version="1.0" encoding="utf-8"?>
-<SOAP-ENV:Envelope xmlns:SOAP-ENV="http://www.w3.org/2003/05/soap-envelope" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-    <SOAP-ENV:Header>
-        <NS1:gnreCabecMsg xmlns:NS1="https://www.gnre.pe.gov.br/webservice/GnreLoteRecepcao">
-            <versaoDados xmlns="http://www.gnre.pe.gov.br/webservice/GnreLoteRecepcao">1.00</versaoDados>
-        </NS1:gnreCabecMsg>
-    </SOAP-ENV:Header>
-    <SOAP-ENV:Body>
-        <processar xmlns=\"https://www.gnre.pe.gov.br/webservice/GnreLoteRecepcao\">
-            <gnreDadosMsg>
-                <TLote_GNRE xmlns=\"http://www.gnre.pe.gov.br\">
-                    <guias>
-                        <TDadosGNRE>
-                            <c01_UfFavorecida>AC</c01_UfFavorecida>
-                            <c02_receita>100099</c02_receita>
-                            <c26_produto>44</c26_produto>
-                            <c27_tipoIdentificacaoEmitente>1</c27_tipoIdentificacaoEmitente>
-                            <c03_idContribuinteEmitente>
-                                <CNPJ>05744574000198</CNPJ>
-                            </c03_idContribuinteEmitente>
-                            <c28_tipoDocOrigem>10</c28_tipoDocOrigem>
-                            <c04_docOrigem>000666</c04_docOrigem>
-                            <c06_valorPrincipal>666</c06_valorPrincipal>
-                            <c10_valorTotal>6</c10_valorTotal>
-                            <c14_dataVencimento>2012-10-10</c14_dataVencimento>
-                            <c16_razaoSocialEmitente>EMPRESA TESTE LTDA</c16_razaoSocialEmitente>
-                            <c18_enderecoEmitente>RUA SHIT</c18_enderecoEmitente>
-                            <c19_municipioEmitente>02704</c19_municipioEmitente>
-                            <c20_ufEnderecoEmitente>PE</c20_ufEnderecoEmitente>
-                            <c21_cepEmitente>13456000</c21_cepEmitente>
-                            <c22_telefoneEmitente>38892984</c22_telefoneEmitente>
-                            <c34_tipoIdentificacaoDestinatario>2</c34_tipoIdentificacaoDestinatario>
-                            <c35_idContribuinteDestinatario>
-                                <CPF>06010296820</CPF>
-                            </c35_idContribuinteDestinatario>
-                            <c37_razaoSocialDestinatario>ZULEYD RIBEIRO</c37_razaoSocialDestinatario>
-                            <c38_municipioDestinatario>02704</c38_municipioDestinatario>
-                            <c33_dataPagamento>2012-09-05</c33_dataPagamento>
-                            <c05_referencia>
-                                <periodo>1</periodo>
-                                <mes>10</mes>
-                                <ano>2012</ano>
-                            </c05_referencia>
-                        </TDadosGNRE>
-                        <TDadosGNRE>
-                            <c01_UfFavorecida>AC</c01_UfFavorecida>
-                            <c02_receita>100099</c02_receita>
-                            <c26_produto>44</c26_produto>
-                            <c27_tipoIdentificacaoEmitente>1</c27_tipoIdentificacaoEmitente>
-                            <c03_idContribuinteEmitente><CNPJ>05744574000198</CNPJ>
-                        </c03_idContribuinteEmitente>
-                        <c28_tipoDocOrigem>10</c28_tipoDocOrigem>
-                        <c04_docOrigem>000666</c04_docOrigem>
-                        <c06_valorPrincipal>666</c06_valorPrincipal>
-                        <c10_valorTotal>6</c10_valorTotal>
-                        <c14_dataVencimento>2012-10-10</c14_dataVencimento>
-                        <c16_razaoSocialEmitente>EMPRESA TESTE LTDA</c16_razaoSocialEmitente>
-                        <c18_enderecoEmitente>RUA SHIT</c18_enderecoEmitente>
-                        <c19_municipioEmitente>02704</c19_municipioEmitente>
-                        <c20_ufEnderecoEmitente>PE</c20_ufEnderecoEmitente>
-                        <c21_cepEmitente>13456000</c21_cepEmitente>
-                        <c22_telefoneEmitente>38892984</c22_telefoneEmitente>
-                        <c34_tipoIdentificacaoDestinatario>2</c34_tipoIdentificacaoDestinatario>
-                        <c35_idContribuinteDestinatario>
-                            <CPF>06010296820</CPF>
-                        </c35_idContribuinteDestinatario>
-                        <c37_razaoSocialDestinatario>ZULEYD RIBEIRO</c37_razaoSocialDestinatario>
-                        <c38_municipioDestinatario>02704</c38_municipioDestinatario>
-                        <c33_dataPagamento>2012-09-05</c33_dataPagamento>
-                        <c05_referencia>
-                            <periodo>1</periodo>
-                            <mes>10</mes>
-                            <ano>2012</ano>
-                        </c05_referencia>
-                    </TDadosGNRE>
-                    <TDadosGNRE>
-                        <c01_UfFavorecida>AC</c01_UfFavorecida>
-                        <c02_receita>100099</c02_receita>
-                        <c26_produto>44</c26_produto>
-                        <c27_tipoIdentificacaoEmitente>1</c27_tipoIdentificacaoEmitente>
-                        <c03_idContribuinteEmitente>
-                            <CNPJ>05744574000198</CNPJ>
-                        </c03_idContribuinteEmitente>
-                        <c28_tipoDocOrigem>10</c28_tipoDocOrigem>
-                        <c04_docOrigem>000666</c04_docOrigem>
-                        <c06_valorPrincipal>666</c06_valorPrincipal>
-                        <c10_valorTotal>6</c10_valorTotal>
-                        <c14_dataVencimento>2012-10-10</c14_dataVencimento>
-                        <c16_razaoSocialEmitente>EMPRESA TESTE LTDA</c16_razaoSocialEmitente>
-                        <c18_enderecoEmitente>RUA SHIT</c18_enderecoEmitente>
-                        <c19_municipioEmitente>02704</c19_municipioEmitente>
-                        <c20_ufEnderecoEmitente>PE</c20_ufEnderecoEmitente>
-                        <c21_cepEmitente>13456000</c21_cepEmitente>
-                        <c22_telefoneEmitente>38892984</c22_telefoneEmitente>
-                        <c34_tipoIdentificacaoDestinatario>2</c34_tipoIdentificacaoDestinatario>
-                        <c35_idContribuinteDestinatario>
-                            <CPF>06010296820</CPF>
-                        </c35_idContribuinteDestinatario>
-                        <c37_razaoSocialDestinatario>ZULEYD RIBEIRO</c37_razaoSocialDestinatario>
-                        <c38_municipioDestinatario>02704</c38_municipioDestinatario>
-                        <c33_dataPagamento>2012-09-05</c33_dataPagamento>
-                        <c05_referencia>
-                            <periodo>1</periodo>
-                            <mes>10</mes>
-                            <ano>2012</ano>
-                        </c05_referencia>
-                    </TDadosGNRE>
-                </guias>
-            </TLote_GNRE>
-        </gnreDadosMsg>
-    </processar>
-</SOAP-ENV:Body>
-</SOAP-ENV:Envelope>';
+$xml = '<?xml version="1.0" encoding="utf-8" standalone="no"?>
+<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+<soap:Header>
+    <gnreCabecMsg xmlns="http://www.gnre.pe.gov.br/webservice/GnreLoteRecepcao">
+        <versaoDados>2.00</versaoDados>
+    </gnreCabecMsg>
+</soap:Header>
+<soap:Body>
+    <gnreDadosMsg xmlns="http://www.gnre.pe.gov.br/webservice/GnreLoteRecepcao">
+        <TLote_GNRE xmlns="http://www.gnre.pe.gov.br">
+            <guias>
+                <TDadosGNRE>
+                    <c01_UfFavorecida>PE</c01_UfFavorecida>
+                    <c02_receita>100145</c02_receita>
+                    <c26_produto></c26_produto>
+                    <c27_tipoIdentificacaoEmitente>1</c27_tipoIdentificacaoEmitente>
+                    <c03_idContribuinteEmitente>
+                        <CNPJ>47368253000186</CNPJ>
+                    </c03_idContribuinteEmitente>
+                    <c06_valorPrincipal>0.01</c06_valorPrincipal>
+                    <c14_dataVencimento>2019-08-21</c14_dataVencimento>
+                    <c15_convenio>49-Protocolo</c15_convenio>
+                    <c16_razaoSocialEmitente>ECIN EMPRESARIAL</c16_razaoSocialEmitente>
+                    <c18_enderecoEmitente>AV ESTER 192</c18_enderecoEmitente>
+                    <c19_municipioEmitente>12803</c19_municipioEmitente>
+                    <c20_ufEnderecoEmitente>SP</c20_ufEnderecoEmitente>
+                    <c21_cepEmitente>13150029</c21_cepEmitente>
+                    <c22_telefoneEmitente>1938722867</c22_telefoneEmitente>
+                    <c34_tipoIdentificacaoDestinatario></c34_tipoIdentificacaoDestinatario>
+                    <c35_idContribuinteDestinatario>
+                        <CNPJ></CNPJ>
+                    </c35_idContribuinteDestinatario>
+                    <c37_razaoSocialDestinatario></c37_razaoSocialDestinatario>
+                    <c38_municipioDestinatario></c38_municipioDestinatario>
+                    <c33_dataPagamento>2019-08-21</c33_dataPagamento>
+                    <c05_referencia>
+                        <mes>08</mes>
+                        <ano>2019</ano>
+                    </c05_referencia>
+                    <c39_camposExtras>
+                        <campoExtra>
+                            <codigo>9</codigo>
+                            <tipo>T</tipo>
+                            <valor>TESTE</valor>
+                        </campoExtra>
+                    </c39_camposExtras>
+                </TDadosGNRE>
+            </guias>
+        </TLote_GNRE>
+    </gnreDadosMsg>
+</soap:Body>
+</soap:Envelope>';
 
- $url = "https://www.testegnre.pe.gov.br/gnreWS/services/GnreLoteRecepcao?wsdl" ;
+$function = 'processar';
 
- $ch = curl_init ();
- curl_setopt ( $ch , CURLOPT_URL, $url);
- curl_setopt ( $ch , CURLOPT_RETURNTRANSFER, true );
- curl_setopt ( $ch , CURLOPT_SSL_VERIFYPEER, false );
- curl_setopt ( $ch , CURLOPT_SSL_VERIFYHOST, 0 );
-
- $headers = array ();
- array_push ( $headers , "Tipo de Conteúdo: text / xml; charset = utf-8" );
- array_push ( $headers , "Aceitar: texto / xml" );
- array_push ( $headers , "Cache-Control: no-cache" );
- array_push ( $headers , "Pragma: no-cache" );
- array_push ( $headers , "SOAPAction: https://www.testegnre.pe.gov.br/gnreWS/services/GnreLoteRecepcao?wsdl" );
- if ( $xml != null ) {
-    curl_setopt ( $ch , CURLOPT_POSTFIELDS, " $xml " );
-    array_push ( $headers , "Content-Length:" . strlen ( $xml ));
- }
- curl_setopt ( $ch , CURLOPT_USERPWD, "user_name:password" );  /* Se necessário */
- curl_setopt ( $ch , CURLOPT_POST, true );
- curl_setopt ( $ch , CURLOPT_HTTPHEADER, $headers );
- $response = curl_exec ( $ch );
- $code = curl_getinfo ( $ch , CURLINFO_HTTP_CODE);
- curl_close ( $ch );
- ?>
+try {
+$opt = array('trace' => true,'encoding' => 'utf8', 'soap_version' => SOAP_1_2);
+$cliente = new SoapClient(null, array(
+    'location' => 'http://www.testegnre.pe.gov.br/gnreWS/services/GnreLoteRecepcao?wsdl',
+    'uri' => 'http://www.testegnre.pe.gov.br/'));
+ 
+$result = $cliente->processar($xml);
+    echo "<pre>";
+    print_r($result);
+} catch (SoapFault $e) {
+    echo $e->getMessage();
+}
+?>
